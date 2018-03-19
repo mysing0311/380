885ec1ff7dafd5dc9a723757b09ef3007e79aec0
student <student@192.168.52.58>
2018-03-19 12:51:25 +0800

    Task 3 done

--- src/main/java/ouhk/comps380f/controller/TicketServlet.java
+++ src/main/java/ouhk/comps380f/controller/TicketServlet.java
@@ -28,6 +28,11 @@ public class TicketServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        if (request.getSession().getAttribute("username") == null) {
+            response.sendRedirect("login");
+            return;
+        }
+
         String action = request.getParameter("action");
         if (action == null) {
             action = "list";
@@ -52,6 +57,11 @@ public class TicketServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        if (request.getSession().getAttribute("username") == null) {
+            response.sendRedirect("login");
+            return;
+        }
+
         String action = request.getParameter("action");
         if (action == null) {
             action = "list";
@@ -100,7 +110,11 @@ public class TicketServlet extends HttpServlet {
     private void createTicket(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         Ticket ticket = new Ticket();
-        ticket.setCustomerName(request.getParameter("customerName"));
+        //ticket.setCustomerName(request.getParameter("customerName"));
+        ticket.setCustomerName(
+                (String) request.getSession().getAttribute("username")
+        );
+        
         ticket.setSubject(request.getParameter("subject"));
         ticket.setBody(request.getParameter("body"));
 
